640750cacf24f489cb2f144a3f3f2a194af9813a
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-16 11:56:26 +0530

    Entering A Valid Last Name

--- Pattern.sh
+++ Pattern.sh
@@ -2,9 +2,22 @@
 
 echo "Welcome To User Registration"
 
+#UC1
+
 read -p "enter a name " name
 Pattern1='^[A-Z]{1}[a-zA-Z]{3,15}$';
 if [[ $name =~ $Pattern1 ]]
+then
+    echo "yes"
+else
+    echo "no"
+fi
+
+#UC2
+
+read -p "enter a surname " surname
+Pattern2='^[A-Z]{1}[a-zA-Z]{3,15}$';
+if [[ $surname =~ $Pattern2 ]]
 then
     echo "yes"
 else
