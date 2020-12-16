f0521141a2fcf5e15840c0e178808c8ca651f293
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-16 12:21:21 +0530

    Entering A Predifined Mobile Format

--- Pattern.sh
+++ Pattern.sh
@@ -2,33 +2,10 @@
 
 echo "Welcome To User Registration"
 
-#UC1
 
-read -p "enter a name " name
-Pattern1='^[A-Z]{1}[a-zA-Z]{3,15}$';
-if [[ $name =~ $Pattern1 ]]
-then
-    echo "yes"
-else
-    echo "no"
-fi
-
-#UC2
-
-read -p "enter a surname " surname
-Pattern2='^[A-Z]{1}[a-zA-Z]{3,15}$';
-if [[ $surname =~ $Pattern2 ]]
-then
-    echo "yes"
-else
-    echo "no"
-fi
-
-#UC3
-
-read -p "enter a emailid " emailid
-Pattern3="^[a-zA-Z._-]+@(gmail|bl|yahoo)+.(com|co|in)";
-if [[ $emailid =~ $Pattern3 ]]
+read -p "enter a mobile no " no
+Pattern4='^((\+)?91)[ ]?[9049]{1}[0-9]{9}$'
+if [[ $no =~ $Pattern4 ]]
 then
     echo "yes"
 else
