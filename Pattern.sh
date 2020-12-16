9c5c37f87b28b265ba828e4fc984f10c80a51bff
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-16 11:50:21 +0530

    Entering A Valid First Name

--- Pattern.sh
+++ Pattern.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome To User Registration"
+
+read -p "enter a name " name
+Pattern1='^[A-Z]{1}[a-zA-Z]{3,15}$';
+if [[ $name =~ $Pattern1 ]]
+then
+    echo "yes"
+else
+    echo "no"
+fi
